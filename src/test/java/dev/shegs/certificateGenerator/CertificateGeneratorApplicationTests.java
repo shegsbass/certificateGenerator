5e9d127dd02faf805e5d49efb6afc8eac2a50e5f
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-02-09 01:47:50 +0100

    Initial commit

---
+++ src/test/java/dev/shegs/certificateGenerator/CertificateGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.shegs.certificateGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
